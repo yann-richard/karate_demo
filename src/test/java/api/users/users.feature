793e3355b167b2623e13e30b652f787ac769aefc
YR <yannrichard20@gmail.com>
2022-03-31 12:31:15 +0200

    2nd commit update framework

--- src/test/java/api/users/users.feature
+++ src/test/java/api/users/users.feature
@@ -1,8 +1,8 @@
-Feature: sample karate test script
-  for help, see: https://github.com/intuit/karate/wiki/IDE-Support
+Feature:
 
   Background:
     * url 'https://jsonplaceholder.typicode.com'
+
 
   Scenario: get all users and then get the first user by id
     Given path 'users'
--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -2,11 +2,18 @@ function fn() {
   var env = karate.env; // get system property 'karate.env'
   karate.log('karate.env system property was:', env);
 
+
   if (!env) {
     env = 'test';
   }
 
    var config = {
+
+//   api {
+// env: env,
+//    /*api_base_url: 'https://ztrain-shop.herokuapp.com',*/
+//    product: '61efb02b4e23dc71cab1331b'
+//   }
       web: {
 //        navigateur: {type: 'chrome'},
 //        zenityIntranetUrl: 'https://intranet-test.zenity-test.fr',
@@ -24,8 +31,6 @@ function fn() {
 //        }
       }
     }
-// var result = karate.callSingle('classpath:com/ztrain_api/user/login.feature@TEST_OF-704', config);
-//  config.authInfo = { token: result.resp.token, user: result.resp.user._id };
 
 //  if (env == 'dev') {
 //    // customize
@@ -33,5 +38,7 @@ function fn() {
 //  } else if (env == 'e2e') {
 //    // customize
 //  }
+//var result = karate.callSingle('classpath:com/ztrain_api/user/login.feature@TEST_OF-704', config);
+//  config.authInfo = { token: result.resp.token, user: result.resp.user._id };
   return config;
 }
